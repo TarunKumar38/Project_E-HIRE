c173c7270cc300f40010826b8b47a87e9f6ffd74
TarunKumar38 <tarunkumar3838@gmail.com>
2021-05-08 12:12:55 +0530

    changed editor

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,41 +1,61 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import io from "socket.io-client";
-
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/theme-tomorrow";
 
 function Editor() {
   const { id: roomId } = useParams();
-	const [text, setText] = useState('');
+  const [text, setText] = useState(`#include <iostream>
+using namespace std;
 
-  console.log(roomId);
-	const socketRef = useRef();
+int main() {
+  
+  cout<<"Hello There";
+  
+  return 0;
+}`);
 
-	useEffect(() => {
+  const socketRef = useRef();
+
+  useEffect(() => {
     socketRef.current = io.connect("http://localhost:5000");
-		
-		socketRef.current.emit("join-room", roomId);
+
+    socketRef.current.emit("join-room", roomId);
     socketRef.current.on("textChanged1", (text) => {
       setText(text);
     });
     return () => socketRef.current.disconnect();
   }, [text]);
 
-
-	const textChanged = (event) => {
-    event.preventDefault();
-    socketRef.current.emit("textChanged", event.target.value);
-    setText(event.target.value);
+  const textChanged = (value) => {
+    socketRef.current.emit("textChanged", value);
+    setText(value);
   };
-	
+
   return (
     <div>
       Editor
       <div className='textarea'>
-        <textarea
-          className='editor'
-          style={{ height: "300px", width: "500px" }}
-          onChange={textChanged}
+        <AceEditor
+          mode='csharp'
+          theme='tomorrow'
+          fontSize={20}
+          height='500px'
+          width='600px'
           value={text}
+          onChange={textChanged}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 4,
+          }}
         />
       </div>
       <div>
